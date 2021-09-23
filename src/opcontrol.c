7e68bbddbf5ef6ea0bf78bb73893b1e5e1800990
Maya Cobb <mayacobb@students.berkeley.net>
2021-09-23 15:07:34 -0700

    commit

--- src/opcontrol.c
+++ src/opcontrol.c
@@ -21,6 +21,9 @@
    Encoder encoder;
    Encoder yeet;
 
+   int counts;
+   int counts2;
+
    int power;
    int turn;
    int shoulderPower;
@@ -29,81 +32,77 @@
    bool is_reversed = false;
    encoder = encoderInit(QUAD_TOP_PORT, QUAD_BOTTOM_PORT, is_reversed);
    yeet = encoderInit(QUAD_TOP_PORTT, QUAD_BOTTOM_PORTT, is_reversed);
-   // ... Do work
-   // Get encoder reading in degrees
-   int counts = encoderGet(encoder);
-   int counts2 = encoderGet(yeet);
 
-   // ... Do other work
-   encoderReset(encoder);
-   encoderReset(yeet);
+   // encoderReset(encoder);
+   // encoderReset(yeet);
 
-   printf("Hello %d", (counts));
-   printf("Hello %d", (counts2));
+   // printf("Hello %d", (counts));
+   // printf("Hello %d", (counts2));
 
-     while (1) {
+   while (1) {
 
-         power = joystickGetAnalog(1, 1); // vertical axis on left joystick
-         turn  = joystickGetAnalog(1, 2); // horizontal axis on right joystick
-         motorSet(3, power + turn); // set left wheels
-         motorSet(2, power - turn); // set right wheels
+       power = joystickGetAnalog(1, 1); // vertical axis on left joystick
+       turn  = joystickGetAnalog(1, 2); // horizontal axis on right joystick
+       motorSet(3, power + turn); // set left wheels
+       motorSet(2, power - turn); // set right wheels
 
 
-         if(joystickGetDigital(1, 8, JOY_UP) == 1) {
-           clawSet(127); // pressing up, so lift should go up
-         }
-         else if(joystickGetDigital(1, 8, JOY_DOWN) == 1) {
-           clawSet(-127); // pressing down, so lift should go down
+       if(joystickGetDigital(1, 8, JOY_UP) == 1) {
+         clawSet(127); // pressing up, so lift should go up
+       }
+       else if(joystickGetDigital(1, 8, JOY_DOWN) == 1) {
+         clawSet(-127); // pressing down, so lift should go down
 
-         }
-         else {
-           clawSet(0);
-         }
+       }
+       else {
+         clawSet(0);
+       }
 
-         // SHOULDER CONTROL
+       // SHOULDER CONTROL
 
-        if(joystickGetDigital(1, 7, JOY_UP) == 1) {
-          shoulderSet(127); // pressing up, so lift should go up
-        }
-        else if(joystickGetDigital(1, 7, JOY_DOWN) == 1) {
-          shoulderSet(-127); // pressing down, so lift should go down
+      if(joystickGetDigital(1, 7, JOY_UP) == 1) {
+        shoulderSet(127); // pressing up, so lift should go up
+      }
+      else if(joystickGetDigital(1, 7, JOY_DOWN) == 1) {
+        shoulderSet(-127); // pressing down, so lift should go down
 
-        }
-        else {
-          shoulderSet(0);
-        }
+      }
+      else {
+        shoulderSet(0);
+      }
 
+      // ELBOW CONTROL
 
-        // ELBOW CONTROL
+      if(joystickGetDigital(1, 6, JOY_UP) == 1) {
+        elbowSet(127); // pressing up, so lift should go up
+      }
+      else if(joystickGetDigital(1, 6, JOY_DOWN) == 1) {
+        elbowSet(-127); // pressing down, so lift should go down
 
-        if(joystickGetDigital(1, 6, JOY_UP) == 1) {
-          elbowSet(127); // pressing up, so lift should go up
-        }
-        else if(joystickGetDigital(1, 6, JOY_DOWN) == 1) {
-          elbowSet(-127); // pressing down, so lift should go down
+      }
+      else {
+        elbowSet(0);
+      }
 
-        }
-        else {
-          elbowSet(0);
-        }
+      // WRIST CONTROL
 
-        // WRIST CONTROL
+      if(joystickGetDigital(1, 5, JOY_UP) == 1) {
+        wristSet(127); // pressing up, so lift should go up
+      }
+      else if(joystickGetDigital(1, 5, JOY_DOWN) == 1) {
+        wristSet(-127); // pressing down, so lift should go down
+      }
+      else {
+        wristSet(0);
+      }
 
-        if(joystickGetDigital(1, 5, JOY_UP) == 1) {
-          wristSet(127); // pressing up, so lift should go up
-        }
-        else if(joystickGetDigital(1, 5, JOY_DOWN) == 1) {
-          wristSet(-127); // pressing down, so lift should go down
-        }
-        else {
-          wristSet(0);
-        }
+      counts = encoderGet(encoder);
+      counts2 = encoderGet(yeet);
+      pot = analogRead(5);
+      printf("the pot value %d \n", pot);
+      printf("the encoder value %d \n", counts);
+      printf("the encoder value %d \n", counts2);
+      delay(600);
 
-        pot = analogRead(5);
-        printf("the pot value %d \n", pot);
-        printf("the encoder value %d \n", counts);
-        printf("the encoder value %d \n", counts2);
-        delay(600);
-
-     }
+    }
 }
