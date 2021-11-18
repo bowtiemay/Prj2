55859c98f21aa74034877a7912233018cb202c5b
Astroknotts <90803143+Astroknotts@users.noreply.github.com>
2021-11-18 15:32:46 -0800

    Update opcontrol.c
    manual update

--- src/opcontrol.c
+++ src/opcontrol.c
@@ -12,10 +12,13 @@
 #include "claw.h"
 #include "elbow.h"
 #include "wrist.h"
+#include "homeshoulder.h"
+#include "homeElbow.h"
+#include "jointcontrol.h"
+#include "math.h"
+#include "straightLine.h"
 
 
-/*
- */
  void operatorControl() {
 
    Encoder encoder;
@@ -29,7 +32,12 @@
    int shoulderPower;
    int pot = 0;
 
-   bool is_reversed = false;
+   int xVal = 2;
+   int zVal = 20; //constant
+   double forearm = 35;
+   double bicep = 27;
+
+   bool is_reversed = true;
    encoder = encoderInit(QUAD_TOP_PORT, QUAD_BOTTOM_PORT, is_reversed);
    yeet = encoderInit(QUAD_TOP_PORTT, QUAD_BOTTOM_PORTT, is_reversed);
 
@@ -40,7 +48,22 @@
    // printf("Hello %d", (counts2));
 
    while (1) {
+      printf("the encoder value %d \n", encoderGet(yeet));
+      printf("the encoder that supposedly works %d \n", encoderGet(encoder));
+      delay(1000);
 
+      if (joystickGetDigital(1, 8, JOY_LEFT) == 1) {
+        homeShoulder(190, encoder);
+        homeElbow(80, yeet);
+      }
+
+      if(joystickGetDigital(1, 8, JOY_RIGHT) == 1){
+
+        double angle2 = determineAngle2(xVal, zVal, forearm, bicep); //stays constant
+        double angle1 = determineAngle1(xVal, zVal, forearm, bicep, angle2); //stays constant
+        xVal = drawLine(angle1, angle2, encoder, yeet, xVal);
+
+      }
        power = joystickGetAnalog(1, 1); // vertical axis on left joystick
        turn  = joystickGetAnalog(1, 2); // horizontal axis on right joystick
        motorSet(3, power + turn); // set left wheels
@@ -52,7 +75,6 @@
        }
        else if(joystickGetDigital(1, 8, JOY_DOWN) == 1) {
          clawSet(-127); // pressing down, so lift should go down
-
        }
        else {
          clawSet(0);
@@ -65,8 +87,8 @@
       }
       else if(joystickGetDigital(1, 7, JOY_DOWN) == 1) {
         shoulderSet(-127); // pressing down, so lift should go down
-
       }
+
       else {
         shoulderSet(0);
       }
@@ -99,10 +121,5 @@
       counts = encoderGet(encoder);
       counts2 = encoderGet(yeet);
       pot = analogRead(5);
-      printf("the pot value %d \n", pot);
-      printf("the encoder value %d \n", counts);
-      printf("the encoder value %d \n", counts2);
-      delay(600);
-
     }
 }
